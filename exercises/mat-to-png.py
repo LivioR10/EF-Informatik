de5ecfb9fa4129c6230ac972b1ed535feca1eefb
LivioR10 <livio.reusser@edu.gbsl.ch>
2026-09-09 09:13:50 +0200

    nix gute

--- exercises/mat-to-png.py
+++ exercises/mat-to-png.py
@@ -2,7 +2,7 @@ import png # Paket png importieren
 faktor = int(input("Zoomfaktor?"))
 smiley = [
     [000, 000, 000, 000, 000, 000],
-    [000, 255, 000, 000, 255, 000],
+    [000, 255, 000, 000, 255, 000], # [000, 000, 255, 255] [000, 000, 000, 000, 255, 255, 255, 255]
     [000, 000, 000, 000, 000, 000],
     [000, 255, 000, 000, 255, 000],
     [000, 000, 255, 255, 000, 000],
@@ -17,24 +17,25 @@ smiley2 = [
     [000, 000, 000, 000, 000, 000]
 ]
 multiplikation = 1
-def verbreiterung(smiley):
-    for z in range(6*multiplikation):
+def verbreiterung(smiley, smiley2):
+    for z in range((len(smiley2[0])-1)*multiplikation):
             k=0
-            for i in range(6*multiplikation):
+            for i in range((len(smiley2[0])-1)*multiplikation):
                 smiley[z].insert(k, smiley2[z][i])
                 k+=2
+    smiley = smiley2
     return smiley, smiley2
-def verdickerung(smiley):
-    verbreiterung(smiley)
+def verdickerung(smiley, smiley2):
+    verbreiterung(smiley, smiley2)
     k=0
-    for z in range(3*multiplikation):
-            smiley[k].insert(k, k)
+    for z in range(len(smiley)*multiplikation):
+            smiley[z].insert(k, smiley2[z])
             k+=2 
-    return smiley
+    return smiley, smiley2
 
 zoom=0
 while zoom<faktor:
-    verdickerung(smiley)
+    verdickerung(smiley, smiley2)
     multiplikation+=1
     zoom+=1
 
